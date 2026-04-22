f81f2aa429bee315a4a5b2ec02fdf0956f26ba46
Verissimo <loverissimo@deloitte.pt>
2026-04-22 10:33:05 +0100

    Update

---
+++ infrastructure/ast.ts
@@ -0,0 +1,44 @@
+import * as azure from "@pulumi/azure-native";
+import { createResource } from "../utils/utils";
+import { config } from "../config";
+import { getTags } from "../tags";
+
+export function createStorageAccount(
+  rg: azure.resources.ResourceGroup,
+  id: string,
+  skuName: string = "Standard_LRS",
+  extraTags?: Record<string, string>
+): azure.storage.StorageAccount {
+
+  return createResource("st", id, (name) => {
+    return new azure.storage.StorageAccount(name, {
+      accountName: name,
+      resourceGroupName: rg.name,
+      location: config.location,
+
+      sku: {
+        name: skuName,
+      },
+
+      kind: "StorageV2",
+
+      tags: getTags(extraTags),
+    });
+  });
+}
+
+export function createBlobContainer(
+  storage: azure.storage.StorageAccount,
+  rg: azure.resources.ResourceGroup,
+  id: string
+): azure.storage.BlobContainer {
+
+  return createResource("blob", id, (name) => {
+    return new azure.storage.BlobContainer(name, {
+      accountName: storage.name,
+      resourceGroupName: rg.name,
+
+      publicAccess: azure.storage.PublicAccess.None,
+    });
+  });
+}
