9a188a7b83c63e2bacaaa78d72898f20c78c4b41
Verissimo <loverissimo@deloitte.pt>
2026-04-24 10:10:08 +0100

    Update

--- config.ts
+++ config.ts
@@ -5,6 +5,7 @@ const cfg = new pulumi.Config();
 export const config = {
   projectName: cfg.get("project") ,
   location: cfg.require("location"),
+  location_short: cfg.get("location_short"),
   environment: pulumi.getStack(),
   subscriptionId: cfg.require("subscriptionId"),
 };
